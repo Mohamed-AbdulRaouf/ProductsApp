a814fa51b9e9b0264df03746ed12428635f5bad9
esraa <esraa.moustafa.9@gmail.com>
2019-05-17 16:54:37 +0200

    add products model

---
+++ ProductsApp/Products.swift
@@ -0,0 +1,50 @@
+//
+//  Products.swift
+//  ProductsApp
+//
+//  Created by Esraa on 5/17/19.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import Foundation
+
+class Products: Codable {
+    let products: [Product]
+    let count: Int
+    
+    init(Products: [Product], count: Int) {
+        self.products = Products
+        self.count = count
+    }
+    
+    enum CodingKeys: String, CodingKey {
+        case products = "data"
+        case count
+    }
+}
+
+class Product: Codable {
+    let id: Int
+    let name, productDescription: String
+    let image: Image
+    let price: Int
+    
+    init(id: Int, name: String, productDescription: String, image: Image, price: Int) {
+        self.id = id
+        self.name = name
+        self.productDescription = productDescription
+        self.image = image
+        self.price = price
+    }
+}
+
+class Image: Codable {
+    let link: String
+    let height, width: String
+    
+    init(link: String, height: String, width: String) {
+        self.link = link
+        self.height = height
+        self.width = width
+    }
+}
