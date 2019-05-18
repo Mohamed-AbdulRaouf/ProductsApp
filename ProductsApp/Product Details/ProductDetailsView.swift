2757c01cf643abd4d339ec070d6ff8f9ebdf0d5d
esraa <esraa.moustafa.9@gmail.com>
2019-05-18 12:53:59 +0200

    arrange app files and add products details vc

---
+++ ProductsApp/Product Details/ProductDetailsView.swift
@@ -0,0 +1,16 @@
+//
+//  ProductDetails.swift
+//  ProductsApp
+//
+//  Created by Esraa on 5/18/19.
+//  Copyright © 2019 example. All rights reserved.
+//
+
+import UIKit
+
+class ProductDetailsView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
